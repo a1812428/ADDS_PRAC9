dc48e3e7e15c8e24237353e862e04cfbc8470902
a1812428 <100541936+a1812428@users.noreply.github.com>
2022-10-10 11:08:23 +0530

    prac 9 solutions

---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isOperator(string x)
+{
+    if (x == "+" || x == "-" || x == "/" || x == "*" || x == "^" || x == "%")
+        return true;
+    else
+        return false;
+}
+
+int evaluatePrefix(vector<string> prefixExpression)
+{
+    stack<int> stack;
+    for (int j = prefixExpression.size() - 1; j >= 0; j--)
+    {
+        if (!isOperator(prefixExpression[j]))
+            stack.push(stoi(prefixExpression[j]));
+        else
+        {
+            int o1 = stack.top();
+            stack.pop();
+            int o2 = stack.top();
+            stack.pop();
+            if(prefixExpression[j] == "+")
+                stack.push(o1 + o2);
+            else if (prefixExpression[j] == "-")
+                stack.push(o1 - o2);
+            else if (prefixExpression[j] == "*")
+                stack.push(o1 * o2);
+            else if (prefixExpression[j] == "/")
+                stack.push(o1 / o2);
+            else if (prefixExpression[j] == "^")
+                stack.push(o1 ^ o2);
+            else if (prefixExpression[j] == "%")
+                stack.push(o1 % o2);
+        }
+    }
+    return stack.top();
+}
+
+string prefixToInfix(vector<string> arr)
+{
+    stack<string> s;
+    int length = arr.size();
+    for (int i = length - 1; i >= 0; i--)
+    {
+        if (isOperator(arr[i]))
+        {
+            string op1 = s.top();
+            s.pop();
+            string op2 = s.top();
+            s.pop();
+            string temp = "(" + op1 + " " + arr[i] + " " + op2 + ")";
+            s.push(temp);
+        }
+        else
+        {
+            s.push(arr[i]);
+        }
+    }
+    return s.top();
+}
+
+int main()
+{
+    vector<string> arr;
+    string prefixExpression;
+    string word;
+    getline(cin, prefixExpression);
+    stringstream ss(prefixExpression);
+    while (ss >> word)
+        arr.push_back(word);
+    string ans = prefixToInfix(arr);
+    ans = ans.substr(1, ans.size() - 2); 
+    int finalAns = evaluatePrefix(arr);
+    cout << ans << " = " << finalAns;
+
+    return 0;
+}
